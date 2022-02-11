8f001648c35040c214016a00d262495567d59a43
Fayed Mauyag <faemauyag13@gmail.com>
2022-02-11 23:25:49 +0800

    added new blade and controller for condo

--- resources/views/layouts/super_admin.blade.php
+++ resources/views/layouts/super_admin.blade.php
@@ -37,7 +37,7 @@
     <style>
         [x-cloak] { display: none !important; }
     </style>
-    
+
     <!-- Styles -->
     @livewireStyles
     @powerGridStyles
@@ -50,7 +50,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
             <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+            <span class="ms-1 font-weight-bold">Super Admin Dashboard</span>
         </a>
         </div>
         <hr class="horizontal dark mt-0">
@@ -70,6 +70,14 @@
                         <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1">Admins</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="{{ route('super-admin.admins') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Condominiums</span>
                 </a>
             </li>
             <li class="nav-item mt-3">
