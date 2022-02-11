cc3ab7cc5fd2ff46a0956d608c7331f1158a9888
Fayed Mauyag <faemauyag13@gmail.com>
2022-02-11 23:43:03 +0800

    Update 2022_02_11_152411_create_condos_table.php

--- database/migrations/2022_02_11_152411_create_condos_table.php
+++ database/migrations/2022_02_11_152411_create_condos_table.php
@@ -15,6 +15,15 @@ class CreateCondosTable extends Migration
     {
         Schema::create('condos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('short_description')->nullable();
+            $table->text('description');
+            $table->double('price');
+            $table->decimal('promo_price')->nullable();
+            $table->boolean('featured')->default(false);
+            $table->string('image')->nullable();
+            $table->text('image_gallery')->nullable();
             $table->timestamps();
         });
     }
